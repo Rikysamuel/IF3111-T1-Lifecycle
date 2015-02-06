0c698acf104de9b6a2502a0f478c00b3c0c7d606
Rikysamuel <rikz.samuel@gmail.com>
2015-02-06 18:50:23 +0700

    Android Life Cycle

---
+++ LifeCycle/LifeCycle1/LifeCycle1/app/src/main/java/com/example/rikysamuel/lifecycle1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.rikysamuel.lifecycle1;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends TracerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ LifeCycle/LifeCycle1/LifeCycle1/app/src/main/java/com/example/rikysamuel/lifecycle1/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.rikysamuel.lifecycle1;
+
+import android.os.Bundle;
+
+public class SecondActivity extends TracerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+
+}
